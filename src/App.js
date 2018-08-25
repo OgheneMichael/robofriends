03a99b07c8c2997dda952f87e24e613224b74a63
OgheneMichael <zonalmeet@gmail.com>
2018-08-25 13:21:15 +0100

    Tweaked to accept SASS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import Scroll from "./Scroll";
-import "./App.css";
+// import "./App.css";
 
 class App extends Component {
 	constructor() {
